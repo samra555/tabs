b2eb426edd50b347ebdcba1be1df0afabd2bee24
Smirnov Kirill <samrarus555@gmail.com>
2022-09-18 15:41:05 +0500

    first commit

---
+++ Food/dist/mainScript.js
@@ -0,0 +1,11 @@
+console.log(123);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.queruSelectorAll(".tabheader__item"),
+    tabsContent = document.queruSelectorAll(".tabcontent"),
+    tabsParent = document.queruSelector(".tabheader__items");
+
+  function hifeTabContent() {
+    tabsContent.forE;
+  }
+});
